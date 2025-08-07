b1cdf299d9e5233ad9d4d7d9254027d1b94f89b2
Salman Farees <salman@magus-re.com>
2025-08-07 12:52:11 +0400

    Update workflow, composer.json, ModuleRegistry, and BacklinksModule

--- includes/Core/ModuleRegistry.php
+++ includes/Core/ModuleRegistry.php
@@ -4,30 +4,57 @@ namespace Magus\Core;
 class ModuleRegistry
 {
     /**
-     * Discover and return all Magus module class names.
+     * Discover all Magus module classes in /includes/Modules/, recursively.
      *
      * @return array
      */
     public static function getModules(): array
     {
         $modules = [];
-        $modulesDir = __DIR__ . '/../modules/';
-        foreach (glob($modulesDir . '*Module.php') as $file) {
-            $class = 'Magus\\Modules\\' . basename($file, '.php');
-            if (class_exists($class)) {
-                $modules[] = $class;
+        $modulesDir = realpath(__DIR__ . '/../Modules/');
+        if (!$modulesDir) {
+            return [];
+        }
+
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($modulesDir)
+        );
+
+        foreach ($iterator as $file) {
+            if (
+                $file->isFile() &&
+                preg_match('/([A-Za-z0-9]+)Module\.php$/', $file->getFilename(), $m)
+            ) {
+                // Example: Seo/SeoModule.php => Magus\Modules\Seo\SeoModule
+                $relativePath = str_replace($modulesDir . DIRECTORY_SEPARATOR, '', $file->getPathname());
+                $parts = explode(DIRECTORY_SEPARATOR, $relativePath);
+                $classPath = [];
+                foreach ($parts as $part) {
+                    if (substr($part, -4) === '.php') {
+                        $classPath[] = basename($part, '.php');
+                    } else {
+                        $classPath[] = $part;
+                    }
+                }
+                // Remove the file extension from the last part
+                $class = 'Magus\\Modules\\' . str_replace(DIRECTORY_SEPARATOR, '\\', implode('\\', $classPath));
+                if (class_exists($class)) {
+                    $modules[] = $class;
+                }
             }
         }
         return $modules;
     }
 
+    /**
+     * Initialize all discovered modules that have an init() method.
+     */
     public static function init()
-{
-    foreach (self::getModules() as $module) {
-        if (method_exists($module, 'init')) {
-            $module::init();
+    {
+        foreach (self::getModules() as $module) {
+            if (method_exists($module, 'init')) {
+                $module::init();
+            }
         }
     }
-}
-
 }
--- includes/Modules/Backlinks/BacklinksModule.php
+++ includes/Modules/Backlinks/BacklinksModule.php
@@ -3,14 +3,10 @@ namespace Magus\Modules\Backlinks;
 
 class BacklinksModule
 {
-    public static function register()
+    // Add a render method (even if just a stub for now)
+    public static function render()
     {
-        // Example: Hook a notice to prove module loads
-        add_action('admin_notices', function () {
-            if (isset($_GET['page']) && $_GET['page'] === 'magus-core-dashboard') {
-                echo '<div class="notice notice-success"><p>Backlinks Module Loaded!</p></div>';
-            }
-        });
-        // You can add CPTs, REST endpoints, etc here
+        // Placeholder output
+        echo '<div>Backlinks Module Rendered</div>';
     }
 }
